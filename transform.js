98fb9b7c4fefb150b8a51dce5e5ee71629cee530
Kevin Kamani <kevinkamani17@gmail.com>
2024-05-13 18:01:07 +0200

    Update transform.js

--- transform.js
+++ transform.js
@@ -1,6 +1,9 @@
 function transform() {
     var inputText = document.getElementById("input").value;
     var transformedText = "";
+    var variables = {};
+    var duplicateVariables = false;
+    var duplicatedNames = {}; // To keep track of duplicate variable names
 
     // Split input into lines
     var lines = inputText.split('\n');
@@ -13,14 +16,53 @@ function transform() {
             if (parts.length === 2) {
                 var varName = parts[0].replace(/%[^.]*\./, '').replace(/\./g, '_').replace(/-/g, '_').replace(/quarkus_/g, 'QUARKUS_').toUpperCase();
                 var value = parts[1].trim();
-                transformedText += varName + ": " + value + "\n";
+                
+                // Check if variable with the same name already exists
+                if (variables.hasOwnProperty(varName)) {
+                    // Check if the value is the same
+                    if (variables[varName] !== value) {
+                        // If values are different, notify the user
+                        duplicateVariables = true;
+                    } else {
+                        // If values are the same, notify the user and mark as duplicated
+                        duplicatedNames[varName] = value;
+                        duplicateVariables = true;
+                    }
+                }
+
+                // Store variable and value
+                variables[varName] = value;
+
+                transformedText += `${varName}: ${value}\n`;
             }
         }
     });
 
     // Display transformed variables
-    document.getElementById("output").innerText = transformedText;
+    var outputText = "";
+    for (var name in variables) {
+        if (!duplicatedNames.hasOwnProperty(name)) {
+            outputText += `${name}: ${variables[name]}\n`;
+        }
+    }
+
+    for (var name in duplicatedNames) {
+        outputText += `${name}: ${duplicatedNames[name]}\n`;
+    }
+
+    document.getElementById("output").innerText = outputText;
+
+    // Display message if duplicate variables were found
+    var messageDiv = document.getElementById("message");
+    if (duplicateVariables) {
+        // Construct message with variable names
+        var duplicateNamesString = Object.keys(duplicatedNames).join(",\n"); // Add line break after each variable name
+        messageDiv.innerHTML = `<center><b>Variables declared multiple times and removed:</b></center> <br>${duplicateNamesString}) `;
+    } else {
+        messageDiv.innerText = ""; // Clear the message
+    }
 }
+
 
 function sortVariables() {
     var outputText = document.getElementById("output").innerText;
